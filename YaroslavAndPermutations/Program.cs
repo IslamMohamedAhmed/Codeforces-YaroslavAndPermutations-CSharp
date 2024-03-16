e93a4261ca5960bbe84a9e51699c07474b95dc96
unknown <islammohamedahmed49@gmail.com>
2024-03-16 17:36:48 +0200

    yaroslav and permutations

---
+++ YaroslavAndPermutations/Program.cs
@@ -0,0 +1,53 @@
+﻿int n,indicator = 0,mx = 0;
+int[] arr1 = new int[1001];
+Array.Clear(arr1, 0, 1001);
+n=Convert.ToInt32(Console.ReadLine());
+string str = Console.ReadLine();
+
+
+if (n == 1)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    var arr2 = str.Split(" ");
+    for (int i = 0; i < n; i++)
+    {
+
+        int x = Convert.ToInt32(arr2[i]);
+        arr1[x]++;
+        mx = Math.Max(mx, x);
+    }
+    for(int i = 0; i <= mx; i++)
+    {
+        if (n % 2 == 0)
+        {
+            if (arr1[i] > n / 2)
+            {
+
+                indicator++;
+                break;
+            }
+        }
+        else
+        {
+            if (arr1[i] > n / 2 + 1)
+            {
+
+                indicator++;
+                break;
+            }
+        }
+   
+    }
+    if(indicator > 0)
+    {
+        Console.WriteLine("NO");
+    }
+    else
+    {
+        Console.WriteLine("YES");
+    }
+}
+
